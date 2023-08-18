2064e6fa9443f34603dd50aa2b61f8797910fbf7
Angel Mucha <angel.mucha@utec.edu.pe>
2023-08-17 21:04:58 -0500

    suma y multi

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 
+@app.route('/suma')
+def suma():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return str(a+b)
+
+@app.route('/multiplicacion')
+def multiplicacion():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return str(a*b)
 def hello():
     return '<h1>Hello, feature2!</h1>'
