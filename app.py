594e9e41b685c0647502b4595a68652f3ad65e2e
LuisFern873 <luis.mendez.l@utec.edu.pe>
2023-08-17 21:05:15 -0500

    feature 2 tarea 2

--- app.py
+++ app.py
@@ -6,4 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello, feature 1!</h1>'
+
+def sumar():
+    param1 = 10
+    param2 = 20
+    return param1 + param2
+
+def multiplicar():
+    param1 = 10
+    param2 = 20
+    return param1 * param2
