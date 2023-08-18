f369a46c122872b847f8b882f2c47c20a95c989e
LuisFern873 <luis.mendez.l@utec.edu.pe>
2023-08-17 21:11:53 -0500

    changes feature 1

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from flask import request
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
     return '<h1>Hello, feature 1!</h1>'
 
+@app.route('/sumar')
 def sumar():
     param1 = 10
     param2 = 20
     return param1 + param2
 
+@app.route('/multiplicar')
 def multiplicar():
     param1 = 10
     param2 = 20
